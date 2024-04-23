668a03e0531560430c6b8f31b4bf2159b326065d
carlos henrique <hencarlos42@gmail.com>
2024-04-23 15:46:59 -0300

    config: adicionando configurações para dockerizar
    e utilitarios como o yarn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
+
+// habilita a coleta de variaveis de ambiente
+config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: process.env.PORT || 5173,
+    strictPort: true
+  }
 })
